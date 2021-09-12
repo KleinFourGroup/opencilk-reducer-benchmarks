96093ff9ddf6778d773fa7a5bb87244d68757cff
Matthew Kilgore <m.kilgore0@gmail.com>
2021-09-12 13:54:06 -0400

    Making output more informative

--- matt-test.sh
+++ matt-test.sh
@@ -1,6 +1,7 @@
 CC=$HOME/opencilk/build/bin/clang
 CPP=$HOME/opencilk/build/bin/clang++
 SENT=$HOME/opencilk/opencilk-project/cheetah/include/cilk/sentinel.h
+PERF=perf.csv
 OPT=-O3
 
 # Default: *100
@@ -23,6 +24,7 @@ NWORKERS=1
 FULLTEST=0
 # Default: 5
 REPS=5
+
 # Overhead codes
 SG=0
 SR=1
@@ -36,6 +38,9 @@ CONFSUF=""
 HEADSEP=';'
 SEP=';'
 ENDROW=''
+SECTION='####'
+COMMENT=' * '
+COMMENT2=' ** '
 
 run_exe()
 {
@@ -95,25 +100,30 @@ run_test()
     then
         printf "$1 compilation failed!\n"
         return 1
+    else
+        printf "${COMMENT}Running test $1 with inputs '%s'... " "${*:4}"
     fi
     ERR=0
-    printf "$1$HEADSEP$CONF$SEP"
-    $2 $3 ${@:4}
+    printf "$1$HEADSEP$CONF$SEP" >> $PERF
+    PARSED=$($2 $3 ${@:4})
     ERR=$(($ERR+$?))
-    printf "$SEP$ERR"
-    printf "%s\n" "$ENDROW"
+    printf "$PARSED$SEP$ERR" >> $PERF
+    printf "runtime: $PARSED; return code: $ERR\n"
+    printf "%s\n" "$ENDROW" >> $PERF
 }
 
 config()
 {
+    echo "$SECTION Runing test | SPA: $1; Inline: $2; Peer: $3 $SECTION"
     CONF=$1"$SEP"$2"$SEP"$3
     CONFSUF=$1$2$3
     make_sentinel $((1-$1)) $3 0 1 1 $2 $2 1
-    # printf "SPA: %s; Inline: %s; Peer: %s\n" "$1" "$2" "$3"
 }
 
 make_sentinel()
 {
+    echo "Making sentinel \"$@\""
+    echo "${COMMENT}Sentinel header at $SENT"
     printf "#ifndef _SENTINEL_TESTS_H\n" > ${SENT}
     printf "#define _SENTINEL_TESTS_H\n" >> ${SENT}
     printf "#define HASH_REDUCER %s\n" "$1" >> ${SENT}
@@ -125,37 +135,51 @@ make_sentinel()
     printf "#define INLINE_FULL_LOOKUP %s\n" "$7" >> ${SENT}
     printf "#define INLINE_ALL_TLS %s\n" "$8" >> ${SENT}
     printf "#endif\n" >> ${SENT}
-    #printf "%s\n" "$1 $2 $3 $4 $5 $6 $7 $8"
 }
 
 build()
 {
-    #echo "Building cheetah" 1>&2
+    LOC="$(pwd)"
+    echo "Building cheetah"
+    echo "${COMMENT}cmake output in $LOC/build_$CONFSUF.txt"
     cd ..
     rm ./build/lib/clang/10.0.1/lib/x86_64-unknown-linux-gnu/libopencilk-abi.bc
-    sh opencilk.sh build
+    sh opencilk.sh build &> $LOC/build_$CONFSUF.txt
     cd stress_test
+}
+
+compile_test_internal()
+{
+    rm -rf $1_$CONFSUF
+    $CC $OPT -g -c -DTIMING_COUNT=$REPS ${@:2} -o $1.o $1.c
+    $CC $OPT -S -DTIMING_COUNT=$REPS ${@:2} -o $1.s $1.c
+    $CC $OPT ${@:2} ktiming.o $1.o -o $1_$CONFSUF
 }
 
 compile_test()
 {
-    rm -rf $1_$CONFSUF
-    $CC $OPT -g -c -DTIMING_COUNT=$REPS -fopencilk -o $1.o $1.c
-    $CC $OPT -S -DTIMING_COUNT=$REPS -fopencilk -o $1.s $1.c
-    $CC $OPT -fopencilk ktiming.o $1.o -o $1_$CONFSUF
+    echo "${COMMENT}Compiling $1"
+    compile_test_internal $1 -fopencilk
 }
 
 compile_cilkscale_test()
 {
+    echo "${COMMENT}Compiling $1 with cilkscale test"
+    compile_test_internal $1 -fopencilk -fcilktool=cilkscale
+}
+
+compile_with_make()
+{
+    echo "${COMMENT}Compiling $1 with Makefile in $2"
     rm -rf $1_$CONFSUF
-    $CC $OPT -g -c -DTIMING_COUNT=$REPS -fopencilk -fcilktool=cilkscale -o $1.o $1.c
-    $CC $OPT -S -emit-llvm -DTIMING_COUNT=$REPS -fopencilk -fcilktool=cilkscale -o $1.ll $1.c
-    $CC $OPT -fopencilk -fcilktool=cilkscale ktiming.o $1.o -o $1_$CONFSUF
+    cd $2; make -s clean; make -s; cd ..
+    cp $2/$1 $1_$CONFSUF
 }
 
 compile()
 {
-    #echo "Compiling tests" 1>&2
+    echo "Compiling tests"
+
     $CC $OPT -g -c -DTIMING_COUNT=$1 -o ktiming.o ktiming.c
     
     compile_test intsum_check
@@ -163,9 +187,7 @@ compile()
     compile_test histogram
     compile_test intlist
     
-    rm -rf bfs_$CONFSUF
-    cd pbfs; make -s clean; make -s; cd ..
-    cp pbfs/bfs bfs_$CONFSUF
+    compile_with_make bfs pbfs
     
     compile_cilkscale_test cilkscale_fib
 
@@ -177,7 +199,13 @@ compile()
     $CC $OPT -fopencilk ktiming.o peer_set_pure_test.o -o peer_set_pure_test_$CONFSUF
 }
 
-clean()
+clean_all()
+{
+    rm -rf build_*.txt perf.csv
+    clean_exe
+}
+
+clean_exe()
 {
     rm -rf intsum_check_*
     rm -rf fib_*
@@ -238,32 +266,17 @@ test_cilkscale_intsum()
     run_test "cilkscale (intsum)" cilkscale_parse cilkscale_intsum $CSCALE_INTSUM
 }
 
-test_peer()
-{
-    if [ ! -e peer_set_pure_test_$CONFSUF ]
-    then
-        printf "vecsum compilation failed!\n"
-        return 1
-    fi
-    printf "vecsum$HEADSEP"
-    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 0
-    printf "$SEP"
-    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 1
-    printf "$SEP"
-    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 2
-    printf "$SEP"
-    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 3
-    printf "%s\n" "$ENDROW"
-}
-
 make_runtime()
 {
-    build &> /dev/null
+    build # &> /dev/null
     compile
 }
 
 run_tests()
 {
+    echo "Running tests"
+    touch $PERF
+    echo "${COMMENT}Performance data in $PERF"
     #test_header $1
     test_intsum
     test_fib
@@ -287,7 +300,7 @@ build_and_test()
 #CILK_NWORKERS=1 ./intsum_check $INTSUM 2
 #CILK_NWORKERS=1 ./fib $FIB 2
 
-clean
+clean_all
 build_and_test 0 0 0
 #build_and_test 0 1 0
 #build_and_test 0 0 1
@@ -296,7 +309,7 @@ build_and_test 0 0 0
 #build_and_test 1 1 0
 #build_and_test 1 0 1
 #build_and_test 1 1 1
-clean
+clean_exe
 
 #Old funcs
 
@@ -363,5 +376,23 @@ test_fib_old()
     else
         printf '$ERR errors!'
     fi
+    printf "%s\n" "$ENDROW"
+}
+
+test_peer()
+{
+    if [ ! -e peer_set_pure_test_$CONFSUF ]
+    then
+        printf "vecsum compilation failed!\n"
+        return 1
+    fi
+    printf "vecsum$HEADSEP"
+    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 0
+    printf "$SEP"
+    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 1
+    printf "$SEP"
+    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 2
+    printf "$SEP"
+    CILK_NWORKERS=1 ./peer_set_pure_test_$CONFSUF $INTSUM 3
     printf "%s\n" "$ENDROW"
 }
